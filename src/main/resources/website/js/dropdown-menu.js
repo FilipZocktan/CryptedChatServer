500b60461e816f6f0100e61ba871f924e58d2a69
Filip Zocktan <dev@filipzocktan.de>
2018-07-27 10:25:26 +0200

    Some code-styling and error fixing

--- src/main/resources/website/js/dropdown-menu.js
+++ src/main/resources/website/js/dropdown-menu.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
-    $('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+    $(".dropdown-menu a.dropdown-toggle").on("click", function (e) {
         var $el = $(this);
         var $parent = $(this).offsetParent(".dropdown-menu");
-        if (!$(this).next().hasClass('show')) {
-            $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+        if (!$(this).next().hasClass("show")) {
+            $(this).parents(".dropdown-menu").first().find(".show").removeClass("show");
         }
         var $subMenu = $(this).next(".dropdown-menu");
-        $subMenu.toggleClass('show');
+        $subMenu.toggleClass("show");
 
-        $(this).parent("li").toggleClass('show');
+        $(this).parent("li").toggleClass("show");
 
-        $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
-            $('.dropdown-menu .show').removeClass("show");
+        $(this).parents("li.nav-item.dropdown.show").on("hidden.bs.dropdown", function (e) {
+            $(".dropdown-menu .show").removeClass("show");
         });
 
-        if (!$parent.parent().hasClass('navbar-nav')) {
+        if (!$parent.parent().hasClass("navbar-nav")) {
             $el.next().css({"top": $el[0].offsetTop, "left": $parent.outerWidth() - 4});
         }
 
@@ -23,5 +23,5 @@ $(document).ready(function () {
 });
 
 function cs() {
-    alert("This feature is comming soon!")
+    alert("This feature is comming soon!");
 }
